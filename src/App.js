44865502ef9719931dbd5ccadef4cc4144a51a1b
Mert Akpınar <mert.akpinar@hotmail.com>
2021-01-19 19:10:26 +0300

    dejan

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header'
+import CharacterTable from './components/CharacterTable'
+import axios from 'axios'
+import React , {useEffect,useState} from 'react'
+import Search from './components/Search'
+
+const hash =  "bd0722d5750b6362d5ba0212ca36726b"
 
 function App() {
+  const[items,setItems] = useState([])
+  const[isLoading,setLoading] = useState(true)
+  const [query,setQuery] = useState('')
+
+  useEffect(()=>{
+      const fetch = async()=>{
+        if(query===''){
+          if(localStorage.getItem('favorites')==='[]'){
+            const result = await axios(`http://gateway.marvel.com/v1/public/characters?ts=1&apikey=344d40df0c8cc373141c1dc321fae9cf&hash=${hash}`)
+            console.log(result.data.data.results)
+            setItems(result.data.data.results)
+            setLoading(false) 
+          }else{
+            let favorite = JSON.parse(localStorage.getItem('favorites'))
+            setItems(favorite)
+            setLoading(false)
+          }
+          
+          
+        }else{
+          const result = await axios(`http://gateway.marvel.com/v1/public/characters?nameStartsWith=${query}&ts=1&apikey=344d40df0c8cc373141c1dc321fae9cf&hash=${hash}`)
+          console.log(result.data.data.results)
+          setItems(result.data.data.results)
+          setLoading(false)
+        }
+      
+    }
+
+    fetch()
+  },[query])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header />
+      <Search search={(q)=>setQuery(q)}></Search>
+      <CharacterTable items={items} isLoading={isLoading} />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from '../img/marvel-logo.png'
+
+const Header = () => {
+    return (
+        <header className="center">
+            <img src={logo} />
+        </header>
+    )
+}
+
+export default Header
